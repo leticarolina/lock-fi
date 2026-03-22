48a2a9101d4ed7d538a56d81821cb965c03b96c4
Leticia Azevedo <leticiac@email.com>
2026-03-22 12:32:35 -0300

    feat: makefile and deploy script

--- smart-contracts/src/LockFi.sol
+++ smart-contracts/src/LockFi.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract SafeVault {
+contract LockFi {
     error AmountZero();
     error InsufficientBalance(uint256 balance);
     error PendingWithdrawExists();
